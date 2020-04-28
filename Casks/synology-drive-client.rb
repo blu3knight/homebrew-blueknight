fde7edb00aed64a6e4359b8f05a939ba60cf9115
Yry German <blueknight@gentoo.org>
2020-04-28 03:14:36 -0400

    Update synology-drive-client.rb
    Fixed Download Path

--- Casks/synology-drive-client.rb
+++ Casks/synology-drive-client.rb
@@ -2,7 +2,7 @@ cask 'synology-drive-client' do
   version '2.0.2-11076'
   sha256 '946e712f410b66333b62d6feef0f52991f69fe000ec12be967651b3adcf3ef4c'
 
-  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/synology-drive-client-11067.dmg"
+  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/Installer/synology-drive-client-11067.dmg"
   appcast 'https://archive.synology.com/download/Tools/SynologyDriveClient/'
   name 'Synology Drive Client'
   homepage 'https://www.synology.com/en-us/releaseNote/SynologyDriveClient'
