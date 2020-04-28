78c077c69c071f0c2e3e44c07fe32f26fbf2dc66
Yry German <blueknight@gentoo.org>
2020-04-28 16:02:59 -0400

    Fix for the File Version. Need to change the official version, until I figure out how to do two versions.
    Signed-off-by: Yry German <blueknight@gentoo.org>

--- Casks/synology-drive-client.rb
+++ Casks/synology-drive-client.rb
@@ -1,8 +1,8 @@
 cask 'synology-drive-client' do
-  version '2.0.2-11076'
+  version '11076'
   sha256 '946e712f410b66333b62d6feef0f52991f69fe000ec12be967651b3adcf3ef4c'
 
-  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/Installer/synology-drive-client-11076.dmg"
+  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/2.0.2-#{version}/Mac/Installer/synology-drive-client-#{version}.dmg"
   appcast 'https://archive.synology.com/download/Tools/SynologyDriveClient/'
   name 'Synology Drive Client'
   homepage 'https://www.synology.com/en-us/releaseNote/SynologyDriveClient'
