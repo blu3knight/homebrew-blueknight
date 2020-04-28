f4a6fb97ab83c5183c46d18af9c11984c605d268
Yry German <blueknight@gentoo.org>
2020-04-28 16:29:58 -0400

    Mistake Version 2.0.1-11061
    Signed-off-by: Yry German <blueknight@gentoo.org>

--- Casks/synology-drive-client.rb
+++ Casks/synology-drive-client.rb
@@ -2,7 +2,7 @@ cask 'synology-drive-client' do
   version '11061'
   sha256 '946e712f410b66333b62d6feef0f52991f69fe000ec12be967651b3adcf3ef4c'
 
-  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/2.0.2-#{version}/Mac/Installer/synology-drive-client-#{version}.dmg"
+  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/2.0.1-#{version}/Mac/Installer/synology-drive-client-#{version}.dmg"
   appcast 'https://archive.synology.com/download/Tools/SynologyDriveClient/'
   name 'Synology Drive Client'
   homepage 'https://www.synology.com/en-us/releaseNote/SynologyDriveClient'
