728b0616a3dcf89dc27db984b9eb678b816b266a
Yry German <blueknight@gentoo.org>
2020-05-13 02:21:05 -0400

    Adding a commercial software but older version. Will not be supported for long - Use notebooks for the actual version
    Signed-off-by: Yry German <blueknight@gentoo.org>

---
+++ Casks/notebooks-bk.rb
@@ -0,0 +1,12 @@
+cask 'notebooks' do
+  version '1.4.5'
+  sha256 '11571a75a89ba5aac2f846e95e3d3520d3c47b034b8df2166e93d98a7dde310a'
+
+#  url 'https://notebooksapp.com/Download/Notebooks.zip'
+  url 'https:'https://binhost.technologysecure.com/homebrew/Notebooks-#{version}.dmg
+  appcast 'https://www.notebooksapp.com/NBMacAppcast.xml'
+  name 'Notebooks'
+  homepage 'https://www.notebooksapp.com/mac/'
+
+  app 'Notebooks.app'
+end
