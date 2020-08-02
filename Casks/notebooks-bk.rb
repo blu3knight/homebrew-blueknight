3f1c8d728a2a5831852aaee0f78497ca3f6f7ea0
BlueKnight <blueknight@gentoo.org>
2020-08-02 00:38:32 -0400

    Update notebooks-bk.rb

--- Casks/notebooks-bk.rb
+++ Casks/notebooks-bk.rb
@@ -3,7 +3,7 @@ cask 'notebooks-bk' do
   sha256 '11571a75a89ba5aac2f846e95e3d3520d3c47b034b8df2166e93d98a7dde310a'
 
 #  url 'https://notebooksapp.com/Download/Notebooks.zip'
-  url 'https://binhost.technologysecure.com/homebrew/Notebooks-#{version}.dmg'
+  url 'http://binhost.technologysecure.com/homebrew/Notebooks-#{version}.dmg'
   appcast 'https://www.notebooksapp.com/NBMacAppcast.xml'
   name 'Notebooks'
   homepage 'https://www.notebooksapp.com/mac/'
