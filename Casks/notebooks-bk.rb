98e21a0780e503325c006d7530bb8adeacc203ba
Yry German <blueknight@gentoo.org>
2021-05-10 17:13:56 -0400

    Changed the URL for the file

--- Casks/notebooks-bk.rb
+++ Casks/notebooks-bk.rb
@@ -3,7 +3,7 @@ cask 'notebooks-bk' do
   sha256 '4bcf8f705cf1911b2b907e188eb0ae22168c3c15cd9384935a6823a3665a73b2'
 
 #  url 'https://notebooksapp.com/Download/Notebooks.zip'
-  url "https://binhost.technologysecure.com/homebrew/Notebooks-#{version}.dmg"
+  url "https://binhost.yuryweb.com/homebrew/Notebooks-#{version}.dmg"
   appcast 'https://www.notebooksapp.com/NBMacAppcast.xml'
   name 'Notebooks'
   homepage 'https://www.notebooksapp.com/mac/'
