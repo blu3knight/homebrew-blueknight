dda4510a3438e3c63bc145c54bc5cae0c86db46c
BlueKnight <blueknight@gentoo.org>
2021-07-04 01:33:15 -0400

    Update synology-assistant.rb
    Needed to change the download URL from tool to utility

--- Casks/synology-assistant.rb
+++ Casks/synology-assistant.rb
@@ -2,7 +2,7 @@ cask "synology-assistant" do
   version "7.0-50029"
   sha256 "1c7c913b056b855ed07bce5e6dafc23b1ea154b1a7fc06ec800affc1cc235ff5"
 
-  url "https://global.download.synology.com/download/Tools/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
+  url "https://global.download.synology.com/download/Utility/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
   appcast "https://archive.synology.com/download/Tools/Assistant/"
   name "Synology Assistant"
   homepage "https://www.synology.com/en-global/knowledgebase/DSM/help/Assistant/assistant"
