02efe880c59cdfb25473aabe21785f6fde0fc2a4
BlueKnight <blueknight@gentoo.org>
2021-07-04 01:29:22 -0400

    Update synology-assistant.rb
    Changed synology-assistant to version 7.0-50029

--- Casks/synology-assistant.rb
+++ Casks/synology-assistant.rb
@@ -1,13 +1,13 @@
-cask 'synology-assistant' do
-  version '6.2-24922'
-  sha256 'b586a41cf390586bdc29ab4d6ee7abbca2e435c74426b194997f35553f1ef7ae'
+cask "synology-assistant" do
+  version "7.0-50029"
+  sha256 "1c7c913b056b855ed07bce5e6dafc23b1ea154b1a7fc06ec800affc1cc235ff5"
 
   url "https://global.download.synology.com/download/Tools/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
-  appcast 'https://archive.synology.com/download/Tools/Assistant/'
-  name 'Synology Assistant'
-  homepage 'https://www.synology.com/en-global/knowledgebase/DSM/help/Assistant/assistant'
+  appcast "https://archive.synology.com/download/Tools/Assistant/"
+  name "Synology Assistant"
+  homepage "https://www.synology.com/en-global/knowledgebase/DSM/help/Assistant/assistant"
 
-  app 'SynologyAssistant.app'
+  app "SynologyAssistant.app"
 
   zap trash: '~/Library/Saved Application State/com.yourcompany.DSAssistant.savedState'
  
