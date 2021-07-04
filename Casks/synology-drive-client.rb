53d7a5cffd5bb9b900edc6c8c4b746968ec58ea4
BlueKnight <blueknight@gentoo.org>
2021-07-04 01:13:24 -0400

    Fixed a minor syntax error

--- Casks/synology-drive-client.rb
+++ Casks/synology-drive-client.rb
@@ -2,7 +2,7 @@ cask "synology-drive-client" do
   version "3.0.1,12664"
   sha256 "2fd2ad2d5ee1de87531e016931dae14c14f44febd33992c156f14c0295dd2485"
 
-  url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client--#{version.after_comma}.dmg"
+  url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
   appcast "https://archive.synology.com/download/Tools/SynologyDriveClient/"
   name "Synology Drive Client"
   homepage "https://www.synology.com/en-us/releaseNote/SynologyDriveClient"
