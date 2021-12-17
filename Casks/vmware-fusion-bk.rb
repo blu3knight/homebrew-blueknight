9f296fa8f69a56cd7321f2335a7b514c067fb837
Yury German <blueknight@gentoo.org>
2021-12-17 11:25:34 -0500

    Needed to fix name

--- Casks/vmware-fusion-bk.rb
+++ Casks/vmware-fusion-bk.rb
@@ -1,4 +1,4 @@
- cask "vmware-fusion" do
+ cask "vmware-fusion-bk" do
    version "11.5.6-16696540"
    sha256 "f26f8404204af4fcd8932789d849a56e96d0dbeab1a76e35e6a2b42f2f31ffc2"
    url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
