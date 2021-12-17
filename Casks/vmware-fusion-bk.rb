446fa6daba8f52b68575836ea27a11a96f9e8d67
Yury German <blueknight@gentoo.org>
2021-12-17 11:13:15 -0500

    Adding VMware Fusion for 11.5.6 as that is needed for macOS Catalina (Version 10.15.7)

---
+++ Casks/vmware-fusion-bk.rb
@@ -0,0 +1,5 @@
+ cask "vmware-fusion" do
+-  version "11.5.6-16696540"
+-  sha256 "f26f8404204af4fcd8932789d849a56e96d0dbeab1a76e35e6a2b42f2f31ffc2"
+   url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
+   appcast "https://softwareupdate.vmware.com/cds/vmw-desktop/fusion.xml"
