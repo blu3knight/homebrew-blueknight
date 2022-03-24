0ca0fedcb66739d661377235887b87aa9f67bd87
Yury German <blueknight@gentoo.org>
2022-03-24 17:16:02 -0400

    Change to local URL due to not able to get binary

--- Casks/synergy-bk.rb
+++ Casks/synergy-bk.rb
@@ -9,7 +9,8 @@ cask "synergy-bk" do
     sha256 "7b0b8f202da8be7ba33dec86db1fce004be029a56f20759388349c480a934b26"
   end
 
-  url "https://api2.prod.symless.com/aws-downloads/synergy/v#{version.csv.first.major}/synergy/v#{version.csv.first.major}-core-standard/#{version.csv.first}-stable.#{version.csv.second}/synergy_#{version.csv.first}-stable.#{version.csv.second}_macos#{arch}.dmg"
+  #url "https://api2.prod.symless.com/aws-downloads/synergy/v#{version.csv.first.major}/synergy/v#{version.csv.first.major}-core-standard/#{version.csv.first}-stable.#{version.csv.second}/synergy_#{version.csv.first}-stable.#{version.csv.second}_macos#{arch}.dmg"
+  url "https://binhost.yuryweb.com/homebrew/synergy_#{version.csv.first}-stable.#{version.csv.second}_macos#{arch}.dmg"
   name "Synergy"
   desc "Keyboard and mouse sharing tool - open-source core"
   homepage "https://symless.com/synergy"
