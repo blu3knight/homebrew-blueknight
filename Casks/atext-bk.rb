7d86918d5ced5711c687305c98522aaaf9cabfd7
Yury German <blueknight@gentoo.org>
2022-03-25 16:30:04 -0400

    aText Version 3.1.1 initial install

---
+++ Casks/atext-bk.rb
@@ -0,0 +1,12 @@
+cask "atext-bk" do
+  version "3.1.1"
+  sha256 "f5e83344baaa890e8be98cab20856de3660c6aad1595c6edd5004dc7aca83ad5"
+
+  url "https://binhost.yuryweb.com/homebrew/aText-#{version}.dmg"
+  name "Atext Version 3"
+  homepage "https://www.trankynam.com/atext/"
+
+
+  app "aText.app"
+
+end
