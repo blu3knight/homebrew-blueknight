2ce2bce531e3611bf71f6452c89e0ef02fa2fd1f
Yury German <blueknight@gentoo.org>
2022-03-29 13:23:37 -0400

    Version 3.1.2

--- Casks/atext-bk.rb
+++ Casks/atext-bk.rb
@@ -1,6 +1,6 @@
 cask "atext-bk" do
-  version "3.1.1"
-  sha256 "f5e83344baaa890e8be98cab20856de3660c6aad1595c6edd5004dc7aca83ad5"
+  version "3.1.2"
+  sha256 "7bed89fffa520a84c6ab4431ab9e9b631da8e9dd8cad194af4a2fbe8fe299558"
 
   url "https://binhost.yuryweb.com/homebrew/aText-#{version}.dmg"
   name "Atext Version 3"
