db8900864451cc2e0d0d70dc3f8f65762417c6a2
Yury German <blueknight@gentoo.org>
2022-04-18 22:25:08 -0400

    Created a version of aText for Version 2.

---
+++ Casks/atext-v2.rb
@@ -0,0 +1,12 @@
+cask "atext-v2" do
+  version "2.40.5"
+  sha256 "4e4ffc07dd4972cf493a89ec8cc0a546a5f4e1629958d9e34e71cd23d4e0e0b2"
+
+  url "https://binhost.yuryweb.com/homebrew/aText-#{version}.dmg"
+  name "Atext Version 2"
+  homepage "https://www.trankynam.com/atext/"
+
+
+  app "aText.app"
+
+end
