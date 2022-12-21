65e53b46ccc25fd31c41fe22aa3544dfa6ce2194
Yury German <blueknight@gentoo.org>
2022-12-21 17:25:44 -0500

    Synology Drive Client - 3.2.1-13271

--- Casks/synology-drive-client.rb
+++ Casks/synology-drive-client.rb
@@ -1,6 +1,6 @@
 cask "synology-drive-client" do
-  version "3.1.0,12923"
-  sha256 "b54f9c588457d1cd5d8b9eeace070a3111add4e81136e261fde9a6156217955f"
+  version "3.2.1,13271"
+#  sha256 "b54f9c588457d1cd5d8b9eeace070a3111add4e81136e261fde9a6156217955f"
 
   url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
   appcast "https://archive.synology.com/download/Tools/SynologyDriveClient/"
