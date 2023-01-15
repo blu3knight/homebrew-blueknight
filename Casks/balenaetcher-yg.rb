b7028f42f6f4cbb45a901b48619a48dce53cc341
Yury German <blueknight@gentoo.org>
2023-01-15 17:10:34 -0500

    Updating the etcher app

--- Casks/balenaetcher-yg.rb
+++ Casks/balenaetcher-yg.rb
@@ -1,16 +1,10 @@
-cask "synology-drive-client" do
+cask "balenaetcher-yg" do
   version "3.2.1,13271"
-  sha256 "6db43fd276471a0dd9d7d8d60c719837d5ef9390210ff6147ec40ccaa9bca8aa"
+#  sha256 "6db43fd276471a0dd9d7d8d60c719837d5ef9390210ff6147ec40ccaa9bca8aa"
+   url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg"
+  name "Balena Etcher"
+  homepage "https://www.balena.io/etcher/"
 
-  url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
-  appcast "https://archive.synology.com/download/Tools/SynologyDriveClient/"
-  name "Synology Drive Client"
-  homepage "https://www.synology.com/en-us/releaseNote/SynologyDriveClient"
-
-  depends_on macos: '>= :el_capitan'
-
-  pkg "Install Synology Drive Client.pkg"
-
-  uninstall pkgutil: "/Applications/Synology Drive Client.app/Contents/SharedSupport/Remove Synology Drive Client.app"
+  pkg "Install balenaEtcher.app"
 
 end
