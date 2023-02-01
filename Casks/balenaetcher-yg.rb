2bf58a021f6b884ff80c138ced6c47d3e0301d3c
Yury German <blueknight@gentoo.org>
2023-01-31 21:24:27 -0500

    Upgrading Balenaetcher to 1.14.3

--- Casks/balenaetcher-yg.rb
+++ Casks/balenaetcher-yg.rb
@@ -1,5 +1,5 @@
 cask "balenaetcher-yg" do
-  version "1.13.3"
+  version "1.14.3"
 #  sha256 :no_check
 
   url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
