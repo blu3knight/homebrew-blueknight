9dc6b437c442e844bcd6f0136490d3592949a521
Yury German <blueknight@gentoo.org>
2023-04-24 16:27:33 -0400

    Synology Assistant

--- Casks/synology-assistant.rb
+++ Casks/synology-assistant.rb
@@ -1,6 +1,6 @@
 cask "synology-assistant" do
-  version "7.0-50029"
-  sha256 "1c7c913b056b855ed07bce5e6dafc23b1ea154b1a7fc06ec800affc1cc235ff5"
+  version "7.0.4-50051"
+  #sha256 "1c7c913b056b855ed07bce5e6dafc23b1ea154b1a7fc06ec800affc1cc235ff5"
 
   url "https://global.download.synology.com/download/Utility/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
   appcast "https://archive.synology.com/download/Tools/Assistant/"
