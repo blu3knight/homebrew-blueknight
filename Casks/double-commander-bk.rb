08161c52b370ddeb3b736575f0145300d62e7fbd
Yury German <blueknight@gentoo.org>
2023-07-14 15:26:54 -0400

    Double commander change

--- Casks/double-commander-bk.rb
+++ Casks/double-commander-bk.rb
@@ -1,6 +1,6 @@
 cask "double-commander-bk" do
-  version "0.9.10-9640"
-  sha256 "1859504e33816db8b19e494dedafa97e3cc90accdb15d9b9ee0c1ad2ba6064d1"
+  version "1.1.0-10668"
+  #sha256 "1859504e33816db8b19e494dedafa97e3cc90accdb15d9b9ee0c1ad2ba6064d1"
 
   url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.qt.x86_64.dmg",
       verified: "downloads.sourceforge.net/doublecmd/"
