0838a4ce5daa5bd042c453e06d0ef072bc3c128c
Yury German <blueknight@gentoo.org>
2025-12-30 11:24:08 -0500

    Update for Testing

--- angy-ip-scanner-bk.rb
+++ angy-ip-scanner-bk.rb
@@ -3,7 +3,7 @@ cask "angry-ip-scanner-bk" do
 
   version "3.9.3"
   sha256 arm:   "e8997d2ff12f7322ecb3dd0b4c4a410187aa8a26ad89772c833af6de781dfd29",
-         #intel: "554c5a14ee32c1ef1812f5382209afee75548c350ed767cf924bc5e0532f403b"
+         # intel: "554c5a14ee32c1ef1812f5382209afee75548c350ed767cf924bc5e0532f403b"
 
   url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-mac#{arch}-#{version}.zip",
       verified: "github.com/angryip/ipscan/"
@@ -11,7 +11,7 @@ cask "angry-ip-scanner-bk" do
   desc "Network scanner"
   homepage "https://angryip.org/"
 
-  #disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  # disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: ">= :big_sur"
 
